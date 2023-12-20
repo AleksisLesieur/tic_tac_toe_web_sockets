607db834e7f3440a7206cf62d7a4a7fcbe9dd653
AleksisLesieur <Aleksisjonaslesieur@gmail.com>
2023-12-20 13:43:59 +0200

    updated project, almost finished!

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class BoxData(BaseModel):
     cell_1_8: Optional[str] = None
     cell_1_9: Optional[str] = None
 
+
 class ConnectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
@@ -56,11 +57,26 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
+
 class GameState:
     def __init__(self):
         self.board = [None] * 9
         self.current_player = 'X'
         self.last_player = None
+        self.first_player = False
+        self.second_player = False
+        self.temporary_id = None
+        # self.player2_id = None
+
+    def set_player_id(self, client_id):
+        if not self.temporary_id:
+            self.temporary_id = client_id
+        # elif not self.player2_id and self.player1_id != client_id:
+        #     self.player2_id = client_id
+
+    def switch_player(self, current_id):
+        self.first_player = self.temporary_id < current_id
+        self.second_player = self.temporary_id >= current_id
 
     def make_move(self, index: int, client_id):
         if self.last_player == client_id:
@@ -71,6 +87,7 @@ class GameState:
             self.current_player = 'O' if self.current_player == 'X' else 'X'
             self.last_player = client_id
             return True
+
         return False
 
     def reset(self):
@@ -79,11 +96,11 @@ class GameState:
 
 game_state = GameState()
 
-# box_data = BoxData()
 
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse('index.html', {"request": request})
+
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
@@ -92,23 +109,20 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive_text()
+            array_data = json.loads(data)
             # add game logic here
-            index = int(data)
+            print(array_data)
+            index = int(array_data[0])
+            received_client_id = int(array_data[1])
+            game_state.set_player_id(received_client_id)
+            game_state.switch_player(received_client_id)
+            print(game_state.first_player, game_state.second_player)
             if game_state.make_move(index, client_id):
-                await manager.broadcast(json.dumps({"board": game_state.board, "currentPlayer": game_state.current_player}))
-            # await manager.send_personal_message(f"You wrote: {data}", websocket)
-            # await manager.broadcast(f"Client #{client_id} says: {data}")
+                await manager.broadcast(json.dumps(
+                    {"board": game_state.board, "currentPlayer": game_state.current_player,
+                     "firstPlayer": game_state.first_player, "secondPlayer": game_state.second_player}))
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        # game_state.board = []
-        # game_state.board = [None] * 9
-        # game_state.current_player = 'X'
         await manager.reset()
+        await manager.broadcast(json.dumps({"type": "disconnection", "message": f"Player {client_id} disconnected"}))
         game_state.reset()
-        await manager.broadcast(f"Client #{client_id} has left the chat")
-
-
-
-
-
-
--- static/script.js
+++ static/script.js
@@ -4,34 +4,54 @@ let currentPlayerFront = null
 
 let currentBox = null
 
-let clientId = Date.now()
+// const modal = document.getElementById("modal");
+
+const firstPlayerModal = document.querySelector('.firstPlayer')
+
+const secondPlayerModal = document.querySelector(".secondPlayer");
+
+let clientId = Date.now() - 1503023345436
 
 let receivedData;
 
-    const  socket = new WebSocket(`ws://localhost:8000/ws/${clientId}`)
+const socket = new WebSocket(`ws://localhost:8000/ws/${clientId}`)
 
-    socket.onopen = function(event) {
-        console.log('WebSocket connection established');
-        console.log(event)
-    };
+socket.onopen = function(event) {
+    console.log('WebSocket connection established');
+    console.log(event)
+    firstPlayerModal.style.display = "none";
+    secondPlayerModal.style.display = "none";
+};
 
-    socket.onmessage = function (event) {
-        receivedData = JSON.parse(event.data)
-        console.log(receivedData)
-        for (let i= 0; i<9; i++) {
-            boxes[i].textContent = receivedData['board'][i]
-        }
-        currentPlayerFront = receivedData['currentPlayer']
-    };
+socket.onmessage = function (event) {
+    receivedData = JSON.parse(event.data)
+    console.log(receivedData)
+    console.log(clientId)
+    // if (receivedData.firstPlayer) {
+    //   // Show modal for disconnection
+    //     firstPlayerModal.style.display = "block";
+    //     secondPlayerModal.style.display = "none";
+    // }
+    // else if (receivedData.secondPlayer) {
+    //     firstPlayerModal.style.display = "none";
+    //     secondPlayerModal.style.display = "block";
+    // }
 
-    socket.onclose = function(e) {
-        console.log('WebSocket connection closed', e);
-    };
+    for (let i = 0; i < 9; i++) {
+        boxes[i].textContent = receivedData['board'][i]
+    }
+    currentPlayerFront = receivedData['currentPlayer']
+};
 
-    socket.onerror = function(err) {
-        console.error('WebSocket error', err);
-        socket.close();
-    };
+socket.onclose = function(e) {
+    console.log('WebSocket connection closed', e);
+    // modal.style.display = "block";
+};
+
+socket.onerror = function(err) {
+    console.error('WebSocket error', err);
+    socket.close();
+};
 
 console.log(boxes[0])
 console.log('boxes above this')
@@ -39,12 +59,10 @@ console.log('boxes above this')
 boxes.forEach(function(element, index ){
     element.addEventListener('click', function(boardData) {
         if (boxes[index].textContent === '') {
-            // let temp = currentPlayer
-            // currentPlayerFront = temp === 'X' ? 'O' : 'X'
             console.log("sending")
-            socket.send(index.toString())
+            // socket.send(index.toString())
+            socket.send(JSON.stringify([index.toString(), clientId.toString()]));
             console.log(boxes[index].textContent)
-            // boardData['explicitOriginalTarget'].textContent = currentPlayerFront
         }
     })
 })
